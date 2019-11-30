8a81fb2a68997ffc219a769e17f5d521dc208bc8
JingFeng <gejinnian224@163.com>
2019-12-01 03:15:11 +0800

    fix: read file content please use getDataFromFile

--- client-slgc/src/test/app/gameStates/GameState_Login/test.lua
+++ client-slgc/src/test/app/gameStates/GameState_Login/test.lua
@@ -1,11 +1,11 @@
 local pb = cocos.pb()
-local content = cc.FileUtils:getInstance():getStringFromFile("res/pb/test.pb")
+local content = cc.FileUtils:getInstance():getDataFromFile("res/pb/test.pb")
 --FYD android 文件,如果是在程序包中,assets目录下,那么是无法用io.open打开的,因为文件在assets.zip中
 local writePath = cc.FileUtils:getInstance():getWritablePath()
 local file = io.open(writePath.."test.pb","wb")
 file:write(content)
 file:close()
-local content1 = cc.FileUtils:getInstance():getStringFromFile(writePath.."test.pb")
+local content1 = cc.FileUtils:getInstance():getDataFromFile(writePath.."test.pb")
 assert(content1 == content,"FALSE=>"..tostring(content1))
 release_print("FYD======>",writePath.."test.pb")
 release_print("AAAAAAAAA",pb.loadfile(writePath.."test.pb"))
