302c4f0a0e967d5a7e7c764183633528fbc1ab99
JingFeng <gejinnian224@163.com>
2020-01-04 15:50:47 +0800

    feat:添加下载完成的回调

--- client-slgc/src/app/common/Downloader.lua
+++ client-slgc/src/app/common/Downloader.lua
@@ -2,6 +2,7 @@ local Downloader = class("Downloader")
 local LUA_CALLBACK_TYPE = {
     PROCESS = 0,
     DOWNLOAD_FAILED = 1,
+    DOWNLOAD_SUCCESS = 2,
 }
 function Downloader:ctor()
 
@@ -10,14 +11,14 @@ end
 --阻塞式下载 savePath :a/b/xxx.xx
 function Downloader:downloadSingleFile(url,savePath,processFunc)
     FYDC.excute("Downloader","createSimgleTask",url,savePath,function(type,info) 
-        -- print("FYD====",type,info)
         if type == LUA_CALLBACK_TYPE.PROCESS then
             print("FYD::process=",info.process)
         elseif type == LUA_CALLBACK_TYPE.DOWNLOAD_FAILED then
-            print("FYD::errormessage=",info.errormessage)
+            print("DOWNLOAD_FAILED::errormessage=",info)
+        elseif type == LUA_CALLBACK_TYPE.DOWNLOAD_SUCCESS then
+            print("DOWNLOAD_SUCCESS====",info)
         end
     end)
-    print("111111111111")
 end
 
 
